0112c519488ae51bb36eabe56872e6771723fd04
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-13 12:55:14 +0300

    added first analysis welcome page

--- django/analysis/views.py
+++ django/analysis/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse('Enter comments URL from Amazon.com: ')
+    #return render(request, 'index.html', {'data':data, 'date_interval':date_interval, 'user_selected': user_selected})
