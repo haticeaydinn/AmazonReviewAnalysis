bb42bb5a8a177f1b99e6e32dc5d8e98c43238a79
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-13 17:17:17 +0300

    data collection is done

--- django/analysis/views.py
+++ django/analysis/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Enter comments URL from Amazon.com: ')
-    #return render(request, 'index.html', {'data':data, 'date_interval':date_interval, 'user_selected': user_selected})
+    #return HttpResponse('Enter comments URL from Amazon.com: ')
+    return render(request, 'index.html')
