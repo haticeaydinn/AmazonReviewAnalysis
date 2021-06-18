34cbf552a7c7f9744d71af8ed7d28eb83e0dd38f
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-19 00:54:47 +0300

    all libraries in req txt and added runtime txt

--- swe/settings.py
+++ swe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import whitenoise as whitenoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,5 +142,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+# heroku edition
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static/'),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# end heroku edition
 
 STATIC_URL = '/static/'
