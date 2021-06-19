e9b34fd7dbb373d78eec552d0c96d0ec343ba597
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-19 13:04:18 +0300

    added unit tests

--- analysis/tests.py
+++ analysis/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+class MainPageGetTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response_analysis = self.client.get('/analysis/')
+        # Check that the response is 200 OK.
+        self.assertEqual(response_analysis.status_code, 200)
