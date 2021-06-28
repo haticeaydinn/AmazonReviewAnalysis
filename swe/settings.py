f293d2846d19625e1bdead4ef768840afb11e46f
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-28 12:50:14 +0300

    added heroku log to settings

--- swe/settings.py
+++ swe/settings.py
@@ -149,6 +149,24 @@ USE_TZ = True
 STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static/'),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 # end heroku edition
 
 STATIC_URL = '/static/'
