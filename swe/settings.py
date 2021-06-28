7c92657d58603b92964d0be88ea9b0c96a5ca02d
haticeaydin <hatice3178@yahoo.com.tr>
2021-06-28 10:35:44 +0300

    conn_max_age parameter added for heroku 500 error

--- swe/settings.py
+++ swe/settings.py
@@ -104,7 +104,8 @@ DATABASES = {
         'USER': 'htckzuke',
         'PASSWORD': 'GBnTkMH3tqTkUcg-SBilkh7XCkX1RZ4V',
         'HOST': 'tai.db.elephantsql.com',
-        'PORT': '5432'
+        'PORT': '5432',
+        'CONN_MAX_AGE': 20
     }
 }
 
